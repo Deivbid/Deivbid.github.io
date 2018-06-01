4f0a6f09fe5a0d22bfd4f21f2e6cdf7c5277878c
David Aparicio <davidaapariciobak@gmail.com>
2018-06-01 18:43:16 -0400

    testimonials fix with Paul

--- src/components/Sections/SectionF.js
+++ src/components/Sections/SectionF.js
@@ -13,14 +13,14 @@ export default class SectionF extends Component {
 				<div class="column is-two-thirds">
 				  <div class="testimonials-carousel">
 					<div class="testimonial-block">
-					  <img src="https://www.nbcstore.com/media/catalog/product/cache/1/image/1000x/040ec09b1e35df139433887a97daa66f/s/t/steve-carrell-best-boss-mug_2_1_3_1.jpg" alt="" class="avatar" />
+					  <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/NafSadh_Profile.jpg/768px-NafSadh_Profile.jpg" alt="" class="avatar" />
 					</div>
 	  
 					<p class="quote">
 					  “David was a real pleasure to work with and we look forward to working with him again. He’s the kind of developer you can trust with a project from start to finish.”
 					</p>
 	  
-					<h1 class="title is-size-5">Carlos Irigoyen</h1>
+					<h1 class="title is-size-5">Paul Keller</h1>
 					<h2 class="subtitle is-size-6">Project Manager, Good Kind</h2>
 				  </div>
 				</div>
